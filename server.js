62086df25b830ebd3d4f311976a989f2e671f529
Abraham E. Tavarez <abetavarez@icloud.com>
2020-08-22 20:43:52 -0400

    socketio

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+const http = require("http");
+const path = require("path");
 const express = require("express");
+const socketio = require("socket.io");
+
 const app = express();
-const PORT = 300 || process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = http.createServer(app);
+const io = socketio(server);
+
+//* Sets static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//* Run when client connects
+io.on("connection", (socket) => {
+  console.log("New websocket connection...");
+});
+
+const PORT = 3000 || process.env.PORT;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
